ff9be83ba73b813298a5747383d960a21959c719
Balakumar3018 <balakumarjkp@gmail.com>
2022-04-01 11:17:28 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -4,28 +4,7 @@ import {products} from "./backend/db/products.js"
 function App() {
   return (
     <div className="App">
-      <h3>testing mockbee API</h3>
-      <div>{products.map(({author,title,price,categoryName})=>(
-        <div>
-        <li key={author}>author :{author}</li>
-        <p key={title}>titile: {title}</p>
-        <p key={price}>price : {price}</p>
-        <p key={categoryName}>Category: {categoryName}</p>
-        <li key={author}>author :{author}</li>
-        <p key={title}>titile: {title}</p>
-        <p key={price}>price : {price}</p>
-        <p key={categoryName}>Category: {categoryName}</p>
-        <li key={author}>author :{author}</li>
-        <p key={title}>titile: {title}</p>
-        <p key={price}>price : {price}</p>
-        <p key={categoryName}>Category: {categoryName}</p>
-        <li key={author}>author :{author}</li>
-        <p key={title}>titile: {title}</p>
-        <p key={price}>price : {price}</p>
-        <p key={categoryName}>Category: {categoryName}</p>
-    
-      ))}
-    </div>
+      
     </div>
   );
 }
